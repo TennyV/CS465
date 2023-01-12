a831565e4820eb9bc7585c8a484750b001392a35
tenny.vongtip@gmail.com <tenny.vongtip@gmail.com>
2023-01-12 16:55:12 -0500

    Express website, Module3

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travel Getaways'});
+    res.render('travel', { title: 'Travel Getaways', trips });
 };
 module.exports = {
     travel
+    
 };
